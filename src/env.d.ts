ff87ef8c9df00d6bdce119413536c3b4e58820bc
jsperez <jsperez@veritran.com>
2025-09-03 16:35:57 -0500

    fix: send notification

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,13 @@
 declare global {
-    interface Window {
-        dataLayer: Record<string, any>[];
-    }
+  interface Window {
+    dataLayer: Record<string, any>[]
+  }
 }
 
-export {};
+// Custom types declarations
+declare var myString: string
+
+// Astro types, not necessary if you already have a `tsconfig.json`
+/// <reference path="../.astro/types.d.ts" />
+
+export {}
