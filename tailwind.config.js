bff25c4d15c20474b2ccf8586cb51aec2769ae10
Juanse Perez <inge.jsperez@gmail.com>
2025-09-02 19:05:05 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.cjs
+module.exports = {
+    content: [
+        './src/**/*.{astro,html,js,jsx,tsx,mdx}', // incluye todos los posibles
+        './public/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
